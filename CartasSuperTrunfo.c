15665b26d3660630aa5be1ea4bdf2c0a2acaa0db
Marconi-Junior <mfranca.jr@hotmail.com>
2025-04-03 13:16:39 +0000

    acrescentado calculos pip percapita e dens. pop.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,13 +3,14 @@
 int main() {
 
   char estado1, estado2;
-  char codigo1[3], codigo2[3];
+  char codigo1[4], codigo2[4];
   char nomeCidade1[50], nomeCidade2[50];
   int populacao1, populacao2;
   float area1, area2;
   float pib1, pib2;
   int ptTuristico1, ptTuristico2;
-  
+  float densPopulacional1, densPopulacional2;
+  float pibPerCapita1, pibPerCapita2;
   //Cadastro da primeira carta.
   printf("Cadastre a primeira carta:\n");
   printf("Digite a letra que representa o estado:\n");
@@ -19,20 +20,23 @@ int main() {
   printf("Digite o nome da cidade:\n");
   scanf("%s", &nomeCidade1);
   printf("Digite o número de habitantes:\n");
-  scanf("%d", &populacao1);
+  scanf(" %d", &populacao1);
   printf("Digite a Área:\n");
-  scanf("%f", &area1);
+  scanf(" %f", &area1);
   printf("Digite o PIB:\n");
-  scanf("%f", &pib1);
+  scanf(" %f", &pib1);
   printf("Digite o número de pontos turísticos:\n");
-  scanf("%d", &ptTuristico1);
-  //Impressão da primeira carta
+  scanf(" %d", &ptTuristico1);
+  //cálculo da densidade populacional e pip per capita da primeira carta
+  densPopulacional1 = (float) populacao1 / area1;
+  pibPerCapita1 = (float) (pib1 * 1000000000) / populacao1;
+  //Resultado da primeira carta
   printf("\nCarta: 01\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f Km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado1, codigo1, nomeCidade1, populacao1, area1, pib1, ptTuristico1);
-  
+  printf("Densidade Populacional: %.2f hab/Km²\nPib per Capita: %.2f reais\n", densPopulacional1, pibPerCapita1);
   //Cadastro da segunda carta.
   printf("\nCadastre a segunda carta:\n");
   printf("Digite a letra que representa o estado:\n");
-  scanf(" %c", &estado2);// erro de leitura, necessário espaço para corrigir
+  scanf(" %c", &estado2);// erro de copilação, necessário espaço para corrigir
   printf("Digite o código da carta:\n");
   scanf("%s", &codigo2);
   printf("Digite o nome da cidade:\n");
@@ -40,13 +44,17 @@ int main() {
   printf("Digite o número de habitantes:\n");
   scanf("%d", &populacao2);
   printf("Digite a Área:\n");
-  scanf("%f", &area2);
+  scanf(" %f", &area2);
   printf("Digite o PIB:\n");
-  scanf("%f", &pib2);
+  scanf(" %f", &pib2);
   printf("Digite o número de pontos turísticos:\n");
   scanf("%d", &ptTuristico2);
-  //Impressão da segunda carta
+  //cálculo da densidade populacional e pip per capita da primeira carta
+  densPopulacional2 = (float) populacao2 / area2;
+  pibPerCapita2 = (float) (pib2 * 1000000000) / populacao2;
+  //Resultado da segunda carta
   printf("\nCarta: 02\nEstado: %c\nCódigo: %s\nNome da Cidade: %s\nPopulação: %d\nÁrea: %.2f Km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Turísticos: %d\n", estado2, codigo2, nomeCidade2, populacao2, area2, pib2, ptTuristico2);
+  printf("Densidade Populacional: %.2f hab/Km²\nPib per Capita: %.2f reais\n", densPopulacional2, pibPerCapita2);
   
 return 0;
-} 
+}
